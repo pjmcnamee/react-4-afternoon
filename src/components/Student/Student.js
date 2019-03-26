de1aaf5377ce2e52192a1e5c72f5c99da568aee7
Patrick McNamee <pjmcnamee@comcast.net>
2019-03-26 14:40:59 -0600

    done now working on Black Diamond

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 export default class Student extends Component {
   constructor() {
     super()
+    this.state = {
+      studentInfo: {}
+    }
 
+  }
+
+  componentDidMount(){
+    Axios.get(`http://localhost:3005/students/${this.props.match.params.id}`).then(res => {
+      this.setState({
+        studentInfo: res.data
+      })
+    }).catch(err => console.log('Student info fetch ', err))
   }
 
   render() {
     return (
       <div className="box">
         <h1>Student</h1>
+        <h1>{this.state.studentInfo.first_name} {this.state.studentInfo.last_name}</h1>
+        <h3>Grade: {this.state.studentInfo.grade}</h3>
+        <h3>Email: {this.state.studentInfo.email}</h3>
       </div>
     )
   }
